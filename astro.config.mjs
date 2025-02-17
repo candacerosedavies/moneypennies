4ad4c6b018190d7be00951deb3c96a3dbe310429
candacerosedavies <candacerosedavies@gmail.com>
2025-02-17 16:03:47 +0000

    added contact form and a new baselayout file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';  // Changed this line
 
+
+
 export default defineConfig({
   integrations: [react(), tailwind()],  // Added tailwind here
 });
+
+
+
