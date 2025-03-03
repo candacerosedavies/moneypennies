a32f7649f8ef8e71033369dc08b832bba9111c23
candacerosedavies <candacerosedavies@gmail.com>
2025-03-03 10:23:19 +0000

    fixed bugs with responsive mixins: separate mixins, global.scss and tailwind.css for tailwind base, component and utility styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,22 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  publicDir: 'public',  // Add this line
+  integrations: [
+    react(),
+    // Configure Tailwind integration properly (wasn't working with new mixin scss files)
+    tailwind({
+      config: { path: './tailwind.config.js' },
+    })
+  ],
+  publicDir: 'public',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // Important! makes mixins available globally in components, use with style tags as per reg CSS
+          additionalData: `@import "./src/styles/mixins.scss";`
+        }
+      }
+    }
+  }
 });
