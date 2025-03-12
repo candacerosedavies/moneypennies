31954b2a033c8df01eb31d37413122d0815afa40
candacerosedavies <candacerosedavies@gmail.com>
2025-03-12 17:12:47 +0000

    Parallax section on home page/ contact page updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 import tailwindcssAria from 'tailwindcss-aria';
 
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'parallax-section': 'url("/images/connect/pexels-didsss-2127522.jpg")',
+      },
+      colors: {
+        'grey-blue': '#607ea8',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+      }
+    },
   },
   plugins: [tailwindcssAria],
 }
